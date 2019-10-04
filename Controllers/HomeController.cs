8cfa1b6582c60aca491e66a6eeab08bc4ea9d90a
Dimitar Ushatov <dimitar.ushatov@egt-bg.com>
2019-10-04 19:17:03 +0300

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Manifest.Models;
+using Manifest.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manifest.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        ApplicationUser m_currentUser;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if(m_currentUser == null)
+                m_currentUser = await _userManager.GetUserAsync(User);
+
+            var users = _userManager.Users
+                .Include(x => x.CommentsFrom)
+                .Select(x => new ApplicationUser {
+                    FbName = x.FbName,
+                    Id = x.Id,
+                    FbProfilePicUrl = x.FbProfilePicUrl,
+                    Email = x.Email,
+                    CommentsFrom = x.CommentsFrom.Where(c => c.FromUserId == m_currentUser.Id).ToList()
+                }).ToList();
+
+            foreach(var c in users)
+            {
+                if(c.CommentsFrom.Count() == 0)
+                    c.CommentsFrom.Add(new ApplicationUserComment{FromUserId = m_currentUser.Id, ToUserId = c.Id});
+            }
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeComment([Bind("FromUserId,ToUserId,Comment")]ApplicationUserComment appComment)
+        {
+            if(ModelState.IsValid)
+            {
+                var found = await _context.UserComments.FindAsync(appComment.FromUserId, appComment.ToUserId);
+                if(found != null)
+                {
+                    found.Comment = appComment.Comment;
+                    _context.UserComments.Update(found);
+                }
+                else
+                {
+                    await _context.UserComments.AddAsync(appComment);
+                }
+
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manifest.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUserComment>()
+                .HasKey( x => new { x.FromUserId, x.ToUserId });
+
+            builder.Entity<ApplicationUserComment>()
+                .HasOne( x => x.FromUser )
+                .WithMany( x => x.CommentsTo)
+                .HasForeignKey(x => x.FromUserId);
+
+            builder.Entity<ApplicationUserComment>()
+                .HasOne( x => x.ToUser )
+                .WithMany( x => x.CommentsFrom)
+                .HasForeignKey( x => x.ToUserId);
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<ApplicationUserComment> UserComments { get; set; }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string FbName { get; set;}
+
+        public string FbProfilePicUrl { get; set; }
+
+        public ICollection<ApplicationUserComment> CommentsTo { get; set; }
+        public ICollection<ApplicationUserComment> CommentsFrom { get; set; }
+    }
+
+    public class ApplicationUserComment 
+    {
+        public string FromUserId { get;set; }
+
+        public string ToUserId { get;set; }
+
+        public ApplicationUser FromUser { get; set; }
+
+        public ApplicationUser ToUser { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
